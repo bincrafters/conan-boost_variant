9de73279f8108fdd9314f0bd48ca638cb7029ca6
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:36 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,23 +3,23 @@ from conans import ConanFile
 
 class BoostVariantConan(ConanFile):
     name = "Boost.Variant"
-    version = "1.65.1"
+    version = "1.66.0"
 
     requires = \
-        "Boost.Assert/1.65.1@bincrafters/testing", \
-        "Boost.Bind/1.65.1@bincrafters/testing", \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Detail/1.65.1@bincrafters/testing", \
-        "Boost.Functional/1.65.1@bincrafters/testing", \
-        "Boost.Math/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Preprocessor/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Type_Index/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Utility/1.65.1@bincrafters/testing"
+        "Boost.Assert/1.66.0@bincrafters/testing", \
+        "Boost.Bind/1.66.0@bincrafters/testing", \
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Detail/1.66.0@bincrafters/testing", \
+        "Boost.Functional/1.66.0@bincrafters/testing", \
+        "Boost.Math/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Preprocessor/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Type_Index/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Utility/1.66.0@bincrafters/testing"
 
     lib_short_names = ["variant"]
     is_header_only = True
@@ -27,9 +27,9 @@ class BoostVariantConan(ConanFile):
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-variant"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 
