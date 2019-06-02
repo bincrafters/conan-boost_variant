7b627df135427ea11805df6ee8a5098960230513
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:09 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,28 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostVariantConan(base.BoostBaseConan):
     name = "boost_variant"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_variant"
-    lib_short_names = ["variant"]
-    header_only_libs = ["variant"]
-    b2_requires = [
-        "boost_assert",
-        "boost_bind",
-        "boost_config",
-        "boost_container_hash",
-        "boost_core",
-        "boost_detail",
-        "boost_integer",
-        "boost_move",
-        "boost_mpl",
-        "boost_preprocessor",
-        "boost_static_assert",
-        "boost_throw_exception",
-        "boost_type_index",
-        "boost_type_traits",
-        "boost_utility"
-    ]
+    version = "1.70.0"
